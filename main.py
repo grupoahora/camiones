4c8964995b2eb45f3263f82b9d7365f08d53ed45
Andres Patiño <aantiinoo@gmail.com>
2024-01-03 10:54:46 -0500

    person const main

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ pygame.init()
 window = pygame.display.set_mode([const.WIDTH, const.HEIGHT])
 pygame.display.set_caption("camiones")
 
-def
+def scale(image, scale):
+    w = image.get_width()
+    h = image.get_height()
+    new_image = pygame.transform.scale(image, (w*scale, h*scale))
+    return new_image
 #Animiaciones
-animaciones = []
+animationsh = []
+animationsv = []
 for i in range (2):
     print(i)
-    img = pygame.image.load(f"assets/images/characters/player/playerh-{i}.png")
-player_image = pygame.image.load("assets/images/characters/player/playerh-1.png")
-player = Person(400, 750, player_image)
+    imgh = pygame.image.load(f"assets/images/characters/player/playerh-{i}.png")
+    imgh = scale(imgh, const.SCALE_PERSON)
+    animationsh.append(imgh)
+    imgv = pygame.image.load(f"assets/images/characters/player/playerv-{i}.png")
+    imgv = scale(imgv, const.SCALE_PERSON)
+    animationsv.append(imgv)
+player_imageh = pygame.image.load("assets/images/characters/player/playerh-0.png")
+player_imageh = scale(player_imageh, const.SCALE_PERSON)
+player_imagev = pygame.image.load("assets/images/characters/player/playerv-0.png")
+player_imagev = scale(player_imagev, const.SCALE_PERSON)
+player = Person(400, 750, animationsh, animationsv)
 #definir variables de movimiento
 move_top = False
 move_down = False
@@ -39,6 +52,14 @@ while run == True:
     #print(f"{delta_x}. {delta_y}")
     #move to player
     player.move(delta_x, delta_y)
+    # Mover al jugador solo si hay un cambio en las coordenadas
+    if delta_x != 0 or delta_y != 0:
+        player.move(delta_x, delta_y)
+        if delta_x != 0:
+            player.updateh()
+        if delta_y != 0:
+            player.updatev()
+    
     player.draw(window)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- person.py
+++ person.py
@@ -1,54 +1,57 @@
 import pygame
 import const
 class Person():
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, animationsh,animationsv):
         self.fliph = False
         self.flipv = False
-        self.image = image
-        self.imageh = image
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        self.image = animationsh[self.frame_index]
+        self.imagev = animationsv[self.frame_index]
+        self.imageh = animationsh[self.frame_index]
+        self.animationsh = animationsh
+        self.animationsv = animationsv
     
-        # Calculamos las nuevas dimensiones manteniendo las proporciones
-        self.nuevo_ancho = int(self.image.get_width() * const.SCALE_PERSON)
-        self.nuevo_alto = int(self.image.get_height() * const.SCALE_PERSON)
-
+        
         # Escalamos la imagen con las nuevas dimensiones
         self.shape = pygame.Rect(0,0,const.WIDTH_PERSON,const.HEIGHT_PERSON)
         self.shape.center =  (x,y)
     
     def draw(self, interfaz):
-        self.image = pygame.transform.scale(self.image, (self.nuevo_ancho, self.nuevo_alto))
+        
         imagen_flip = pygame.transform.flip(self.image, self.fliph, self.flipv)
         interfaz.blit(imagen_flip, self.shape)
         #pygame.draw.rect(interfaz, const.COLOR_PERSON, self.shape)
     def move(self, delta_x, delta_y):
         if delta_x < 0:
             self.image = self.imageh
-            # Calculamos las nuevas dimensiones manteniendo las proporciones
-            self.nuevo_ancho = int(self.image.get_width() * const.SCALE_PERSON)
-            self.nuevo_alto = int(self.image.get_height() * const.SCALE_PERSON)
-            
             self.fliph = True
         if delta_x > 0:
             self.image = self.imageh
-            # Calculamos las nuevas dimensiones manteniendo las proporciones
-            self.nuevo_ancho = int(self.image.get_width() * const.SCALE_PERSON)
-            self.nuevo_alto = int(self.image.get_height() * const.SCALE_PERSON)
-            
             self.fliph = False
         if delta_y < 0:
-            self.image = pygame.image.load("assets/images/characters/player/playerv-0.png")
-            # Calculamos las nuevas dimensiones manteniendo las proporciones
-            self.nuevo_ancho = int(self.image.get_width() * const.SCALE_PERSON)
-            self.nuevo_alto = int(self.image.get_height() * const.SCALE_PERSON)
+            self.image = self.imagev
             self.flipv = False
 
         if delta_y > 0:
-            self.image = pygame.image.load("assets/images/characters/player/player-3.png")
-            # Calculamos las nuevas dimensiones manteniendo las proporciones
-            self.nuevo_ancho = int(self.image.get_width() * const.SCALE_PERSON)
-            self.nuevo_alto = int(self.image.get_height() * const.SCALE_PERSON)
-
+            self.image = self.imagev
             self.flipv = True
 
         self.shape.x = self.shape.x + delta_x
         self.shape.y = self.shape.y + delta_y
+    def updateh(self):
+        cooldown_animationh = const.SPEED_ANIMATION_PERSON
+        self.image = self.animationsh[self.frame_index]
+        if pygame.time.get_ticks() - self.update_time >= cooldown_animationh:
+            self.frame_index = self.frame_index + 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animationsh):
+            self.frame_index = 0
+    def updatev(self):
+        cooldown_animationv = const.SPEED_ANIMATION_PERSON
+        self.image = self.animationsv[self.frame_index]
+        if pygame.time.get_ticks() - self.update_time >= cooldown_animationv:
+            self.frame_index = self.frame_index + 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animationsv):
+            self.frame_index = 0
